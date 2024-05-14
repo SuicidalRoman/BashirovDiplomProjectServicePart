89694042750c4aa1992882f6653c675953e2693c
saliery <bashirov.roman.the.best@gmail.com>
2024-05-15 01:15:22 +0300

    Added a simple test method

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,4 +2,16 @@ import pytest
 from sqlalchemy import insert, select
 
 from conftest import client, async_session_maker
+from auth.database import User, UserStatus
 
+async def test_register():
+    response = client.post("/auth/register", json={
+        "email": "test@gmail.com",
+        "password": "testPassword123",
+        "user_status": UserStatus.CLIENT.value,
+        "is_active": True,
+        "is_superuser": False,
+        "is_verified": False,
+    })
+
+    assert response.status_code == 201
