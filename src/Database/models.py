c783c3765062d4ae8c81d4392458218546b0b965
saliery <bashirov.roman.the.best@gmail.com>
2024-06-05 20:13:52 +0300

    Database Changes

--- src/Database/models.py
+++ src/Database/models.py
@@ -18,6 +18,16 @@ class EventType(enum.Enum):
     CONFERENCE = "conference"
     EXHIBITION = "exhibition"
 
+class RequestStatus(enum.Enum):
+    PENDING = "pending"
+    APPROVED = "approved"
+    REJECTED = "rejected"
+
+class RegistrationStatus(enum.Enum):
+    REGISTERED = "registered"
+    ATTENDED = "attended"
+    CANCELLED = "cancelled"
+
 
 users = Table(
     "users",
@@ -26,8 +36,8 @@ users = Table(
     Column("user_login", String, unique=True, nullable=False),
     Column("hashed_password", String, nullable=False),
     Column("user_status", Enum(UserStatus), nullable=False, default=UserStatus.CLIENT),
-    Column("registered_at", TIMESTAMP, default=datetime.utcnow),
-    Column("updated_at", TIMESTAMP, default=datetime.utcnow),
+    Column("registered_at", TIMESTAMP, default=datetime.utcnow()),
+    Column("updated_at", TIMESTAMP, default=datetime.utcnow()),
     Column("is_active", Boolean, default=True, nullable=False),
     Column("is_superuser", Boolean, default=False, nullable=False),
     Column("is_verified", Boolean, default=False, nullable=False)
@@ -53,9 +63,34 @@ requests = Table(
     metadata,
     Column("request_id", Integer, primary_key=True),
     Column("user_id", Integer, ForeignKey("profiles.profile_id"), nullable=False),
-    Column("created_at", TIMESTAMP, default=datetime.utcnow),
-    Column("updated_at", TIMESTAMP, default=datetime.utcnow),
+    Column("created_at", TIMESTAMP, default=datetime.utcnow()),
+    Column("updated_at", TIMESTAMP, default=datetime.utcnow()),
     Column("request_title", String, nullable=False),
-    Column("event_title", String, nullable=False),
-    Column("event_type", Enum(EventType), nullable=False, default=EventType.CONFERENCE)
+    Column("request_description", String, nullable=True),
+    Column("event_id", Integer, ForeignKey("events.id"), nullable=False)
+)
+
+events = Table(
+    "events",
+    metadata,
+    Column("event_id", Integer, primary_key=True),
+    Column("request_id", Integer, ForeignKey("requests.request_id"), nullable=False),
+    Column("title", String, nullable=False),
+    Column("description", String, nullable=True),
+    Column("event_type", Enum(EventType), nullable=False, default=EventType.CONFERENCE),
+    Column("start_timestamp", TIMESTAMP, nullable=False),
+    Column("end_timestamp", TIMESTAMP, nullable=False),
+    Column("created_at", TIMESTAMP, default=datetime.utcnow()),
+    Column("updated_at", TIMESTAMP, default=datetime.utcnow()),
+)
+
+registrations = Table(
+    "registrations", 
+    metadata,
+    Column("registration_id", Integer, primary_key=True),
+    Column("user_id", Integer, ForeignKey("profiles.profile_id"), nullable=False),
+    Column("event_id", Integer, ForeignKey("events.event_id"), nullable=False),
+    Column("status", Enum(RegistrationStatus), nullable=False, default=RegistrationStatus.REGISTERED),
+    Column("registered_at", TIMESTAMP, default=datetime.utcnow()),
+    Column("updated_at", TIMESTAMP, default=datetime.utcnow())
 )
